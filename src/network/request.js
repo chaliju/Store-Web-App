b84b0d8781ad15c828cde84fabab0c052c9cb6cb
chaliju <73051263+chaliju@users.noreply.github.com>
2021-08-13 14:08:12 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -4,7 +4,7 @@ export function request(config) {
   // 1.创建axios的实例
   const instance = axios.create({
     // 真实接口请联系coderwhy老师
-    baseURL: 'http://152.136.185.210:7878/api/m5',
+    baseURL: 'xxx',
     timeout: 5000
   })
 
